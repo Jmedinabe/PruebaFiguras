0defe865747b2b89c8a437638b3bcd165f38c2da
Jmedinabe <jmedinabe@unal.edu.co>
2024-03-14 12:17:49 -0500

    Main

---
+++ src/main/java/com/mycompany/pruebafiguras/Cuadrado.java
@@ -0,0 +1,14 @@
+package com.mycompany.pruebafiguras;
+public class Cuadrado {
+    int lado; // Atributo que define el lado de un cuadrado
+    public Cuadrado(int lado) {
+    this.lado = lado;
+    }
+    double calcularArea() {
+    return lado*lado;
+    }
+    double calcularPerímetro() {
+    return (4*lado);
+    }
+    
+}
---
+++ src/main/java/com/mycompany/pruebafiguras/Rectángulo.java
@@ -0,0 +1,16 @@
+package com.mycompany.pruebafiguras;
+public class Rectángulo {
+    int base;
+    int altura;
+    Rectángulo(int base, int altura) {
+    this.base = base;
+    this.altura = altura;
+    }
+    double calcularArea() {
+    return base * altura;
+    }
+    double calcularPerímetro() {
+    return (2 * base) + (2 * altura);
+    
+}
+}
